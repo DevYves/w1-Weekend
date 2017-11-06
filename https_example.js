21e1fe92de895dd9c443e039b8a844d658705468
DevYves <yfreypons@gmail.com>
2017-11-06 01:38:23 +0000

    added chunk parsing

--- https_example.js
+++ https_example.js
@@ -1,4 +1,5 @@
 var https = require('https');
+var utf8 = require('utf8');
 
 console.log('I did it!');
 
@@ -8,8 +9,13 @@ var options = {
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
